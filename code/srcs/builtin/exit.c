7869ef307752656249ba566db0c5f59d6d81ac32
valktaelen <alix.artiges@hotmail.fr>
2022-02-04 01:06:45 +0100

    Makefile EN && exit builtin

---
+++ code/srcs/builtin/exit.c
@@ -0,0 +1,20 @@
+#include "../../includes/mini_shell.h"
+
+/**
+ * @brief function thatexit the current process with the status given
+ * 
+ * @param shell if set, the function will free the environnement of the
+ * minishell
+ * @param status exit code of the process
+ * @return ** void
+ */
+void	ft_exit(t_shell *shell, int status)
+{
+	if (shell)
+	{
+		ft_track_free_all(shell->t_pars);
+		ft_track_free_all(shell->t_env);
+	}
+	write(1, "exit\n", 5);
+	exit(status);
+}
