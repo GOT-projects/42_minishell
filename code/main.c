4f063fb574b80d93210f224ece67378e42e5d6a7
waxcoin <waxcoin@hotmail.com>
2022-02-05 23:44:52 +0100

    update track_free_tab add len of allocation in minishell and fix unset

--- code/main.c
+++ code/main.c
@@ -30,8 +30,8 @@ int	ft_exec_builtin(t_shell *env, char*cmd, char *args)
 		conv = ft_lst_to_tab(env->envp);
 		ft_track_tab((void **)conv, env->t_env);
 		ft_print(conv);
+		ft_track_free_tab(env->t_env, (void **)conv);
 	}
-		
 	return (1);
 }
 
@@ -41,6 +41,7 @@ int	main(int ac, char **av, char **ev)
 	t_shell	*env;
 	char	*line;
 	char	**args;
+	char	*mini_shell;
 
 	ac = 10 + 1;
 	if (ac == 100 && av)
@@ -52,16 +53,20 @@ int	main(int ac, char **av, char **ev)
 	printf("\e[1;1H\e[2J");
 	while (1)
 	{
-		line = readline("🔞 Minishell> ");
+		printf("env node: %d ", env->t_env->len);
+		mini_shell = ft_track(ft_get_pwd(env->envp), env->t_env);
+		line = readline(mini_shell);
 		if (line[0] != '\0' && line)
 		{
-			args = ft_split(line, ' ');
+			args = ft_strsplit(line, ' ');
 			ft_track_tab((void **)args, env->t_env);
-			free(line);
 			if (ft_strcmp(args[0], "exit") == 0)
 				ft_exit(env, 0);
 			ft_exec_builtin(env, args[0], args[1]);
+			ft_track_free_tab(env->t_env, (void *)args);
 		}
+		free(line);
+		ft_track_free(env->t_env, (void *)mini_shell);
 	}
 	ft_track_free_all(env->t_env);
 	free(env);
