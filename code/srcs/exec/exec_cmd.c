a40fb0cef008d1d5cf5cafc4e81f52383a04e6e2
valktaelen <alix.artiges@hotmail.fr>
2022-02-14 15:05:50 +0100

    exec cmd compatibilité

--- code/srcs/exec/exec_cmd.c
+++ code/srcs/exec/exec_cmd.c
@@ -24,7 +24,7 @@ int	ft_exec_cmd(t_shell *shell, char **cmd)
 	else if (!strcmp("export", cmd[0]))
 		shell->last_exit_status = ft_export(shell, &(cmd[1]));
 	else if (!strcmp("env", cmd[0]))
-		shell->last_exit_status = ft_env(shell, &(cmd[1]));
+		shell->last_exit_status = ft_env(shell);
 	else if (!strcmp("unset", cmd[0]))
 		shell->last_exit_status = ft_unset(shell, &(cmd[1]));
 	else
