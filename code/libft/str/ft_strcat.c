b78a09a33a2f65f751148ccf6f9fdaf6920c985b
waxcoin <waxcoin@hotmail.com>
2022-02-15 22:52:42 +0100

    add fonction ft_joint utilise dans main start fonction des recuperation de var

---
+++ code/libft/str/ft_strcat.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmilhas <jmilas@student.42lyon.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 18:13:31 by jmilhas           #+#    #+#             */
+/*   Updated: 2022/02/04 20:25:18 by aartiges         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+char	*ft_strcat(char *dest, const char *src)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j])
+		dest[i++] = src[j++];
+	dest[i++] = '\0';
+	return (dest);
+}
