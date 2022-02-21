24191c1afbb70ef5fb940646a355d9d3e8681d8c
valktaelen <alix.artiges@hotmail.fr>
2022-02-21 16:03:19 +0100

    ..

--- code/srcs/parsing/tree/cmd.c
+++ code/srcs/parsing/tree/cmd.c
@@ -1,17 +1,5 @@
 #include "../../../includes/mini_shell.h"
 
-void	ft_tolower_str(char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str && str[i])
-	{
-		str[i] = ft_tolower(str[i]);
-		++i;
-	}
-}
-
 /**
  * @brief split the command (prepare for execve)
  * 
@@ -28,8 +16,6 @@ int	ft_construct_cmd(t_shell *shell, t_operation *current)
 		return (1);
 	if (!ft_track_tab((void **)split, &(shell->t_pars)))
 		return (1);
-	if (!strchr(split[0], '/'))
-		ft_tolower_str(split[0]);
 	current->cmd = split;
 	return (0);
 }
